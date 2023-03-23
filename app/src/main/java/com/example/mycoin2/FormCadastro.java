11d95c21605384047ee4150d4a987628e2c4e3c5
thiagodcamill0 <thiagodcamillo@gmail.com>
2023-03-23 06:02:26 -0300

    app mycoin 23/03/2023

--- app/src/main/java/com/example/mycoin2/FormCadastro.java
+++ app/src/main/java/com/example/mycoin2/FormCadastro.java
@@ -1,4 +1,18 @@
 package com.example.mycoin2;
 
-public class FormCadastro {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
+
+    }
+
 }
